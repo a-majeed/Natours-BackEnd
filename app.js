697c30b11d90bf7d5456957f034caa3fe1207876
Azaan <azaan@ualberta.ca>
2021-08-19 17:56:41 -0600

    handling DELETE request

--- app.js
+++ app.js
@@ -89,6 +89,20 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id *1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(204).json({
+    status: 'success',
+    data: null
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port} ..`);
