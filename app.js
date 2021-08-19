79e0d34693135b04b90e024f48a6cc004e3e1f10
Azaan <azaan@ualberta.ca>
2021-08-19 14:55:08 -0600

    starting api and handling GET requests

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req,res) => {
-    res
-        .status(200)
-        .json({ message: 'Hello from the server side!', app: 'Natours' });
+// app.get('/', (req,res) => {
+//     res
+//         .status(200)
+//         .json({ message: 'Hello from the server side!', app: 'Natours' });
 
-})
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length, 
+        data: {
+            tours
+        }
+    });
+});
 
 
 app.listen(port, () => {
