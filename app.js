862893a994045968266f51de8b35329aba2c26ae
Azaan <azaan@ualberta.ca>
2021-08-19 17:25:18 -0600

    responding to URL parameters (id)

--- app.js
+++ app.js
@@ -28,12 +28,34 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id *1;
+
+    if(id > tour.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: "Invalid ID"
+        });
+
+    }
+    const tour = tours.find(el => el.id === id) // only the element equal to the specified parameters will get returned and stored in tour
+    res.status(200).json({
+        status: 'success',
+        results: tours.length, 
+        data: {
+            tour
+        }
+    });
+});
+
 
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
 
     const newId = tours[tours.length-1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
+    const newTour = Object.assign({id: newId}, req.body); // creating a new object by merging existing objects together
 
     tours.push(newTour);
 
