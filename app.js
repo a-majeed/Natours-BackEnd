531a22a33807a73c74d0971b8909fd7694340bb9
Azaan <azaan@ualberta.ca>
2021-08-19 17:41:24 -0600

    handling PATCH requests

--- app.js
+++ app.js
@@ -51,6 +51,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
 });
 
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id *1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+    status: 'success',
+    data: {
+        tour: '<Updated tour here...>'
+        }
+    })
+
+})
+
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
 
