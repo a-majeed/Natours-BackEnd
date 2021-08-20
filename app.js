28e077563e0b797fc40cd0249a05145c9267e8cf
Azaan <azaan@ualberta.ca>
2021-08-20 17:54:47 -0600

    working with environment variables

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('dev')); 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next) => {
     console.log('Hello from the middleware!')
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const app = require('./app')
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env'});
+
 
 const port = 3000;
 app.listen(port, () => {
