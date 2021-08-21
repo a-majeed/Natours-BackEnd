68ee9d84d3153f6f5ba68aad0d05e7194d227ded
Azaan <azaan@ualberta.ca>
2021-08-21 15:08:25 -0600

    seting up db and mongoose

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const app = require('./app')
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env'});
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', 
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true, 
+    useFindAndModify: false
+}).then(con => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+})
+
 
 
 const port = 3000;
