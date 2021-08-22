f5379f9368ed44e80fc85e0c28d1f7877c0a7398
Azaan <azaan@ualberta.ca>
2021-08-21 20:50:54 -0600

    updated controllers to work with mongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,58 +1,110 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req,res,next) => {
-    if(!req.body.name || !req.body.price){
-        return res.status(400).json({
+// exports.checkBody = (req,res,next) => {
+//     if(!req.body.name || !req.body.price){
+//         return res.status(400).json({
+//             status: 'fail',
+//             message: 'missing name or price'
+//         });
+//     }
+//     next();
+// }
+
+exports.getAllTours = async (req, res) => {
+    try{
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            results: tours.length, 
+            data: {
+                tours
+            }
+        });
+    } catch(err){
+        res.status(404).json({
             status: 'fail',
-            message: 'missing name or price'
+            message: err
+        })
+    }
+};
+
+exports.getTour =  async (req, res) => {
+    try{
+        const tour = await Tour.findById(req.params.id);
+
+        res.status(200).json({
+            status: 'success',
+            results: tours.length, 
+            data: {
+                tour
+            }
+     });       
+    } catch (err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
         });
     }
-    next();
 }
 
-exports.getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        // results: tours.length, 
-        // data: {
-        //     tours
-        // }
-    });
-};
+exports.updateTour = async (req, res) => {
+    try{
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true, 
+            runValidators: true
+        });
 
-exports.getTour =  (req, res) => {
-    const tour = tours.find(el => el.id === id) // only the element equal to the specified parameters will get returned and stored in tour
-    // res.status(200).json({
-    //     status: 'success',
-    //     results: tours.length, 
-    //     data: {
-    //         tour
-    //     }
-    // });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+     });       
+    } catch (err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-    status: 'success',
-    data: {
-        tour: '<Updated tour here...>'
-        }
-    });
+exports.createTour = async (req, res) => {
+    try{
 
-};
-
-exports.createTour =  (req, res) => {
-    res.status(201).json({
-        status: "success", 
-        // data: {
-        //     tour: newTour
-        // }
-    })
+        const newTour = await Tour.create(req.body)
+        res.status(201).json({
+            status: "success", 
+            data: {
+                tour: newTour
+            }
+        });  
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
     res.status(204).json({
     status: 'success',
     data: null
     });
 };
+
+exports.deleteTour = async (req, res) => {
+    try{
+        const tour = await Tour.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'success',
+            data: null
+     });       
+    } catch (err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+}
