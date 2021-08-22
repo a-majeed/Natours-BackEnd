f6a20934ca8c4b2cf887dd241baefc63d47db6d1
Azaan <azaan@ualberta.ca>
2021-08-21 21:40:50 -0600

    adding data to model for tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,8 +4,29 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String, 
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     }, 
+    duration: {
+        type: Number, 
+        required:  [true, 'A tour must have a duration']
+    },
+    maxGroupSize : {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingQuantity : {
+        type: Number,
+        default: 0
+    },
+    ratingAverage : {
+        type: Number,
+        default: 4.5
+    },
     rating: {
         type: Number, 
         default: 4.5
@@ -13,6 +34,27 @@ const tourSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
+    },
+    priceDiscount : Number, 
+    summary: {
+        type: String, 
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    description: {
+        type: String, 
+        trim: true
+    },
+    imageCover: {
+        type: String, 
+        required: [true, 'A tour must have a cover image']
+    },
+    images: {
+        type: [String], 
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        }
     }
 });
 
