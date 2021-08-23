9e8a6c9feaac6a5c92248925ce720b807cf8af16
Azaan <azaan@ualberta.ca>
2021-08-22 18:46:55 -0600

    improving api with sorting, limiting fields and filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,7 +12,44 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try{
-        const tours = await Tour.find();
+        
+        // BUILD QUERY
+        // 1) FILTERING
+        const queryObj = { ...req.query};
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        // 1B) ADVANCED FILTERING
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 2) SORTING
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } 
+        else {
+            query = query.sort('-createdAt');
+
+        }
+
+        // 3) FIELD LIMITING 
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        }
+        else{
+            query = query.select('-__v');
+        }
+
+        // 4) PAGINATION
+        // page=2&limit=10, 1-10 page 1, 11-20 page2, 21-30 page 3
+        query = query.skip(2).limit(10)
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length, 
