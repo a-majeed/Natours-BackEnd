5da147fde8f63ce84c94e20bb8b346163657e526
Azaan <azaan@ualberta.ca>
2021-08-25 19:16:06 -0600

    catching errors outside of express

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const app = require('./app')
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// Catching uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+
 dotenv.config({ path: './config.env'});
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>', 
@@ -17,8 +24,16 @@ mongoose.connect(DB, {
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port} ..`);
+});
+
+// Errors outside of express
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    server.close( () => {
+        process.exit(1);
+    })
 });
 
