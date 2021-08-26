ca7c626154d712132a7a9580c063f59322b6306d
Azaan <azaan@ualberta.ca>
2021-08-26 14:59:59 -0600

    fixed typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ process.on('uncaughtException', err => {
     console.log('UNCAUGHT EXCEPTION! Shutting down...');
     console.log(err.name, err.message);
     process.exit(1);
-  });
+});
 
 dotenv.config({ path: './config.env'});
 const app = require('./app');
