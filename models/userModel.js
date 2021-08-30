21b99121558592c123bcb2c18d7d99f91ecc2f0e
Azaan <azaan@ualberta.ca>
2021-08-30 14:27:36 -0600

    adding bcrypt module

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name, email, photo, password and passwordConfirm
 
@@ -19,14 +20,43 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please provide a password'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // this only works on CREATE and SAVE!!
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: "Passwords are not the same"
+        }
     },
-})
+});
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function(next) {
+    // "this" refers to current user
+    // only run if passwod was modified
+    if(!this.isModified('password')) return next();
+
+    // hash the password with cost of 12
+    this.password =  await bcrypt.hash(this.password, 12)
+
+    // delete the passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+});
+
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword,
+    userPassword
+  ) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+  };
+
+  const User = mongoose.model('User', userSchema);
 
 module.exports = User;
