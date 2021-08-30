3d9fdbd3067345b106d5579959303ce5e52a2151
Azaan <azaan@ualberta.ca>
2021-08-30 14:28:27 -0600

    implementing error handling for jwt

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -19,6 +19,8 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 };
 
+const handleJWTError = err => new AppError('Invalid token. Please login again.', 401);
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -67,6 +69,7 @@ module.exports = (err, req, res, next) => {
       if (error.code === 11000) error = handleDuplicateFieldsDB(error);
       if (error.name === 'ValidationError')
         error = handleValidationErrorDB(error);
+      if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
   
       sendErrorProd(error, res);
     }
