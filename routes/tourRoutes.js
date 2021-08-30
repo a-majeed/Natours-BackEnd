da7239d7f22e3f701b3a7a3ea724e95ce3f5c14e
Azaan <azaan@ualberta.ca>
2021-08-30 14:24:47 -0600

    adding authorization ptoection to routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
 // router.param('id', tourController.checkID);
 
@@ -20,7 +21,7 @@ router
     
 router
     .route('/')
-    .get(tourController.getAllTours)
+    .get(authController.protect, tourController.getAllTours)
     .post(tourController.createTour);
 
 router
