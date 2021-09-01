47e628f66578f614b60588c72ea7d5d875b8f920
Azaan <azaan@ualberta.ca>
2021-09-01 14:16:42 -0600

    added jwt error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -19,7 +19,11 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 };
 
-const handleJWTError = err => new AppError('Invalid token. Please login again.', 401);
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -70,6 +74,7 @@ module.exports = (err, req, res, next) => {
       if (error.name === 'ValidationError')
         error = handleValidationErrorDB(error);
       if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+      if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
   
       sendErrorProd(error, res);
     }
