5ceec0adb7d3302b345a8bd6a07c2b7bbe590ec0
Azaan <azaan@ualberta.ca>
2021-09-03 19:07:14 -0600

    adding indexes to improve search performance

--- models/tourModel.js
+++ models/tourModel.js
@@ -37,7 +37,8 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         default: 4.5,
         min: [1, 'Rating must be above 1.0'],
-        max: [5, 'Rating must be below 5.0']
+        max: [5, 'Rating must be below 5.0'],
+        set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7
     },
     rating: {
         type: Number, 
@@ -119,6 +120,11 @@ const tourSchema = new mongoose.Schema({
     toObject: {virtuals: true}
 }
 );
+
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
 
 tourSchema.virtual('durationWeeks').get( function () {
     return this.duration / 7;
