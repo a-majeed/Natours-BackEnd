ead06ca841f260fc153fdb119497ae2fe2096b5d
Azaan <azaan@ualberta.ca>
2021-09-03 19:02:15 -0600

    importing users and reviews into database

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('./../../models/tourModel')
+const Tour = require('./../../models/tourModel');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
 
 dotenv.config({ path: './config.env'});
 
@@ -17,34 +19,41 @@ mongoose.connect(DB, {
 }).then(con => console.log('DB connection successful'));
 
 // READ JSON FILE
-
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
-//IMPORT DATA INTO DB
+// IMPORT DATA INTO DB
 const importData = async () => {
-    try{
+    try {
         await Tour.create(tours);
-        console.log('Data successfuly loaded!');
-        process.exit();
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
+        console.log('Data successfully loaded!');
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-}
+    process.exit();
+};
 
-// DELETE ALL DATA FROM DB 
-
-const deletetData = async () => {
-    try{
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+    try {
         await Tour.deleteMany();
-        console.log('Data successfuly deleted!');
-        process.exit();
+        await User.deleteMany();
+        await Review.deleteMany();
+        console.log('Data successfully deleted!');
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
 }
 
-if(process.argv[2] === '--import'){
-    importData();
-} else if (process.argv[2] === '--delete'){
-    deletetData();
-}
