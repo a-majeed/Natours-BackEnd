2f5d1e6463a4abdf15c84425796f427267667ca1
Azaan <azaan@ualberta.ca>
2021-09-03 19:10:51 -0600

    adding reviews route and modifying who has access to what

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,8 @@ const reviewRouter = require('./../routes/reviewRoutes');
 
 // router.param('id', tourController.checkID);
 
+router.use('/:tourId/reviews', reviewRouter);
+  
 router
     .route('/top-5-cheap')
     .get(tourController.aliasTopTours,tourController.getAllTours)
@@ -18,21 +20,42 @@ router
 
 router
     .route('/monthly-plan/:year')
-    .get(tourController.getMonthlyPlan);
-    
+    .get(
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide', 'guide'),
+      tourController.getMonthlyPlan
+    );
+  
+router
+    .route('/tours-within/:distance/center/:latlng/unit/:unit')
+    .get(tourController.getToursWithin);
+  // /tours-within?distance=233&center=-40,45&unit=mi
+  // /tours-within/233/center/-40,45/unit/mi
+  
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
     .route('/')
-    .get(authController.protect, tourController.getAllTours)
-    .post(tourController.createTour);
+    .get(tourController.getAllTours)
+    .post(
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide'),
+      tourController.createTour
+    );
+
 
 router
     .route('/:id')
     .get(tourController.getTour)
-    .patch(tourController.updateTour)
+    .patch(
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide'),
+      tourController.updateTour
+    )
     .delete(
-        authController.protect, 
-        authController.restrictTo('admin', 'lead-guide'), 
-        tourController.deleteTour);
-
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide'),
+      tourController.deleteTour
+    );
 module.exports = router;
 
