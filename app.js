1d8e6c38d907c95f0d003891a87b8ea4fbd27e25
Azaan <azaan@ualberta.ca>
2021-09-07 15:39:26 -0600

    mounted view router

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -5,16 +6,25 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -33,6 +43,8 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -54,15 +66,6 @@ app.use(
     })
 );
 
-
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
-app.use((req,res,next) => {
-    console.log('Hello from the middleware!')
-    next();
-});
-
 // Test middleware
 app.use((req,res,next) => {
     req.requestTime = new Date().toISOString();
@@ -71,6 +74,7 @@ app.use((req,res,next) => {
 
 
 // 3) ROUTES mounting
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
@@ -83,3 +87,5 @@ app.use(globalErrorHandler);
 
 
 module.exports = app;
+
+//https://documenter.getpostman.com/view/4237486/S1LwxnaE#intro
