e44860fc73335d3e6c72cda3d7f9f6ebca32c44f
Azaan <azaan@ualberta.ca>
2021-09-08 16:57:51 -0600

    added slug parameter

--- models/tourModel.js
+++ models/tourModel.js
@@ -13,6 +13,7 @@ const tourSchema = new mongoose.Schema({
         minlength: [10, 'A tour name must have more or equal then 10 characters']
         // validate: [validator.isAlpha, 'Tour name must only contain characters']
     }, 
+    slug: String,
     duration: {
         type: Number, 
         required:  [true, 'A tour must have a duration']
@@ -29,20 +30,16 @@ const tourSchema = new mongoose.Schema({
             message: 'Difficulty is either: easy, medium, difficult'
           }
     },
-    ratingQuantity : {
+    ratingsQuantity : {
         type: Number,
         default: 0
     },
-    ratingAverage : {
+    ratingsAverage : {
         type: Number,
         default: 4.5,
         min: [1, 'Rating must be above 1.0'],
         max: [5, 'Rating must be below 5.0'],
         set: val => Math.round(val * 10) / 10 // 4.666666, 46.6666, 47, 4.7
-    },
-    rating: {
-        type: Number, 
-        default: 4.5
     },
     price: {
         type: Number,
